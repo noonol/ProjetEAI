2ee45ee262844e60d862f5a32075b8eae9c8affb
DESKTOP-FUHR9P4\Marine <marine.sutarik@gmail.com>
2017-11-16 17:36:28 +0100

    Modification contrat : mis à jour du package (sinon erreur)

--- ProjetEAI-ejb/src/java/messages/Contrat.java
+++ ProjetEAI-ejb/src/java/messages/Contrat.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package bd_eai;
+package messages;
 
 import enumeration.EnumCommunication;
 import enumeration.EnumSecurite;
